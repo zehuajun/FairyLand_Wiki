0f00b99af3e311e1e7b2a9cb9ca277e2a23c6751
zehuajun <zehuajun@zehuajun.top>
2024-05-31 00:04:18 +0800

    完善文档站设置

--- README.md
+++ README.md
@@ -1,21 +1,7 @@
 # FairyLand 文档
 
-此文档站使用 [Docusaurus](https://docusaurus.io/) 构建。
-
-### 安装
-
-```
-$ npm install
-```
-
-### 本地开发
-
-```
-$ npm run serve
-```
-
-此命令启动本地开发服务器并打开浏览器窗口。大多数更改都是实时热重载的，无需重新启动服务器。
+此文档站使用 [Docusaurus](https://docusaurus.io/) 构建，使用 [Obsidian](https://obsidian.md/) 进行编写。
 
 ### 构建部署
 
-使用 [Cloudflare Pages](https://developers.cloudflare.com/pages/) 自动构建部署。
+本文档站使用 [Cloudflare Pages](https://developers.cloudflare.com/pages/) 自动构建部署。
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,30 +3,22 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'FairyLand 文档站',
+  tagline: 'FairyLand 服务器的文档站',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: 'https://docs.fairyland.zehuajun.top/',
   baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'zehuajun',
+  projectName: 'FairyLand_Docs',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'zh-Hans',
+    locales: ['zh-Hans'],
   },
 
   presets: [
@@ -35,17 +27,13 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/zehuajun/FairyLand_Docs/tree/dev/docs/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/zehuajun/FairyLand_Docs/tree/dev/blog/',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -55,7 +43,6 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
       title: 'My Site',
