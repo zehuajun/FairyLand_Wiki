863f94bbf28b99efa51dc67c54a5a60644928665
ZeHuaJun <zehuajun@zehuajun.top>
2024-06-11 22:21:10 +0800

    更新 index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs">
+            to="/docs/intro">
             查看 Wiki
           </Link>
           <Link
